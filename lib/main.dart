35a866ee20b55094b3280a6385cb1ac5eb7f33d9
Nikita Berezovskii <nikber-x@yandex-team.ru>
2023-04-15 14:52:05 +0300

    Fix analyzer error

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,6 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
