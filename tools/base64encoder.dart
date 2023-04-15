08a8a0d3e8ebe7ff25470f2310bfb30dc8c31670
Nikita Berezovskii <nikber-x@yandex-team.ru>
2023-04-15 10:54:34 +0300

    base64encodetool

--- tools/base64encoder.dart
+++ tools/base64encoder.dart
@@ -1,1 +1,16 @@
-void main(List<String> arguments) {}
+import 'dart:convert';
+import 'dart:io';
+
+void main(List<String> arguments) {
+  var modifiedPath = arguments.first;
+  if (arguments.isNotEmpty) {
+    if (arguments.first[0] != '/') {
+      modifiedPath = '/${arguments.first}';
+    }
+  }
+  final fileToEncode =
+      File.fromUri(Uri.parse('${Directory.current.path}$modifiedPath'));
+
+  final base64String = base64Encode(fileToEncode.readAsBytesSync());
+  print(base64String);
+}
